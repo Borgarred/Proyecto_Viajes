129d0233fde3b3ec21d9a88f2ec88c4a3678c2ab
Borja Garramiola Redondo <92300453+Borgarred@users.noreply.github.com>
2023-05-15 21:56:54 +0200

    Update Main.java

--- Aplicación_Viajes_final/src/Aplicacion_Viajes/Main.java
+++ Aplicación_Viajes_final/src/Aplicacion_Viajes/Main.java
@@ -11,8 +11,7 @@ public abstract class Main {
 		menu_viajes viajes = new menu_viajes();
 
 		int indice_usuario = 0;
-		boolean salir = false, salir1 = false, salir2 = false, salir3 = false;
-		
+		boolean salir = false, salir1 = false, salir2 = false, salir3 = false, salir4 = false;
 
 		while (salir == false) {
 
@@ -20,7 +19,7 @@ public abstract class Main {
 
 				Menu1.MenuInicio();
 				if (Menu1.opcion1 != 1 && Menu1.opcion1 != 2) {
-					System.err.println("Elige una opción correcta \n");
+					System.err.println("Elija una de las opcióne correctamente \n");
 				}
 
 				switch (Menu1.opcion1) {
@@ -45,7 +44,7 @@ public abstract class Main {
 				}
 				}
 			} catch (Exception e) {
-				System.err.println("Introduce una opción válida");
+				System.err.println("Introduce una opción válida, (1 o 2)");
 				System.out.println();
 			}
 		}
@@ -55,21 +54,22 @@ public abstract class Main {
 
 			try {
 
+				Menu1.opcion3 = 0;
 				Menu1.MenuViajes(indice_usuario, usuario0);
 				System.out.println();
 				if (Menu1.opcion2 < 1 || Menu1.opcion2 > 4) {
-					System.err.println("Introduzca una opción valida");
+					System.err.println("Introduzca una opción valida entre 1º y 4º");
 
 				}
 				switch (Menu1.opcion2) {
 
 				case 1: {
 					boolean resultado = false;
-					
-					while(resultado == false) {
+
+					while (resultado == false) {
 						resultado = viajes.menuDestino();
-						
-						if(resultado == true)
+
+						if (resultado == true)
 							viajes.menuFecha();
 					}
 					break;
@@ -82,9 +82,43 @@ public abstract class Main {
 					viajes.menuSorteo();
 					break;
 				}
-				
+
 				case 4: {
-					usuario0.DatosUsuario();
+
+					while (salir4 == false) {
+
+						Menu1.MenuUsuario();
+
+						switch (Menu1.opcion3) {
+
+						case 1: {
+							usuario0.DatosUsuario();
+							break;
+						}
+
+						case 2: {
+							usuario0.CambiarDatos();
+							break;
+						}
+
+						case 3: {
+							break;
+						}
+
+						case 4: {
+							usuario0.EliminarUsuario();
+							break;
+						}
+
+						case 5: {
+							System.out.println();
+							salir4 = true;
+							break;
+						}
+
+						}
+					}
+					break;
 				}
 				case 5: {
 					System.out.print("¡Gracias por visitarnos, hasta pronto!");
@@ -98,5 +132,7 @@ public abstract class Main {
 				System.out.println();
 			}
 		}
+
 	}
+
 }

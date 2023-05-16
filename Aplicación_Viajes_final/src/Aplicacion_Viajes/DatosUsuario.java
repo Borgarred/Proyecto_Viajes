c0ef44a0036ec97cf11f1b46102afac40c2b3345
Borja Garramiola Redondo <92300453+Borgarred@users.noreply.github.com>
2023-05-16 14:28:31 +0200

    Update DatosUsuario.java

--- Aplicación_Viajes_final/src/Aplicacion_Viajes/DatosUsuario.java
+++ Aplicación_Viajes_final/src/Aplicacion_Viajes/DatosUsuario.java
@@ -1,48 +1,49 @@
 package Aplicacion_Viajes;
+
 public class DatosUsuario {
 
 	protected String email;
-    protected String contraseña;
-    protected String nombre;
-    protected String apellidos;
-    protected String DNI;
-    protected int edad;
-	
+	protected String contraseña;
+	protected String nombre;
+	protected String apellidos;
+	protected String DNI;
+	protected int edad;
+
 	public DatosUsuario() {
-		
+
 	}
-	
+
 	public DatosUsuario(String email, String contraseña, String nombre, String apellidos, String DNI, int edad) {
-    	this.email = email;
-    	this.contraseña = contraseña;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.DNI = DNI;
-        this.edad = edad;
-    }
-	
-    public String getNombre() {
-        return this.nombre;
-    }
-    
-    public String getApellido() {
-        return this.apellidos;
-    }
-    
-    public String getEmail() {
-        return this.email;
-    }
-    
-    public String getContraseña() {
-        return this.contraseña;
-    }
-    
-    public String getDNI() {
-        return this.DNI;
-    }
-    
-    public int getEdad() {
-        return this.edad;
-    }
+		this.email = email;
+		this.contraseña = contraseña;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.DNI = DNI;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getApellido() {
+		return this.apellidos;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public String getContraseña() {
+		return this.contraseña;
+	}
+
+	public String getDNI() {
+		return this.DNI;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
 
 }

27154116d5da1cbbe7bd7939e0b5f67b1b113250
lopezordonezrebeca <60574419+lopezordonezrebeca@users.noreply.github.com>
2023-05-17 17:23:02 +0200

    Update DatosViajes.java

--- Aplicación_Viajes_final/src/Aplicacion_Viajes/DatosViajes.java
+++ Aplicación_Viajes_final/src/Aplicacion_Viajes/DatosViajes.java
@@ -1,9 +1,9 @@
 package Aplicacion_Viajes;
 
 import java.util.Date;
-
+//Esta clase se usa para guardar los viajes en el array
 public class DatosViajes {
-	
+	//Atributos DatosViajes
 	protected Usuario usuario;
 	protected String localidad;
 	protected String comunidad;
@@ -11,10 +11,10 @@ public class DatosViajes {
 	protected String FechaIda;
 	protected Date hora;
 
-	public DatosViajes() {
+	public DatosViajes() {//Constructor de la clase sin atributos
 		
 	}
-	
+	//Constructor de la clase con atributos
 	public DatosViajes(Usuario usuario2, String comunidad, String localidad, String FechaIda, String FechaVuelta, Date hora) {
 		this.usuario = usuario2;
 		this.comunidad = comunidad;
